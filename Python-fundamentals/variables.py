ab8c5d6e649fd50a7bf5576f86b61a7f73eddb89
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2024-12-23 14:00:49 +0000

    update

---
+++ Python-fundamentals/variables.py
@@ -0,0 +1,21 @@
+''' Variables'''
+
+x = 1  # int
+y = 2.5  # float
+name = "selen"  # string
+is_cool = True  # bool
+
+# Multiple assigment
+
+x, y, name, is_cool = (2, 2.5, "selen", True)
+
+print(x, y, name)
+
+# check type
+print(type(x))
+
+
+# Casting
+
+x = str(x)
+print(type(x))
