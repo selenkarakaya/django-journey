10a124c868d9a9575385a6d9de8a0d9414d54522
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2024-12-24 15:24:02 +0000

    update

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -1,34 +1,8 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getTickets, reset } from "../features/ticket/ticketSlice";
-
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
 import Ticket from "../components/Ticket";
 
 function Tickets() {
-  const { tickets, isLoading, isSuccess } = useSelector(
-    (state) => state.tickets
-  );
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    return () => {
-      if (isSuccess) {
-        dispatch(reset());
-      }
-    };
-  }, [dispatch, isSuccess]);
-
-  useEffect(() => {
-    dispatch(getTickets());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   return (
     <>
       <BackButton />
@@ -40,9 +14,9 @@ function Tickets() {
           <div>Status</div>
           <div></div>
         </div>
-        {tickets.map((ticket) => (
+        {/* {tickets.map((ticket) => (
           <Ticket key={ticket._id} ticket={ticket} />
-        ))}
+        ))} */}
       </div>
     </>
   );
