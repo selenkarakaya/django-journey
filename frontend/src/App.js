a82f2229b412478546d45514fa5ecf249b1830cd
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2024-12-24 15:13:07 +0000

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import BookNow from "./pages/BookNow";
+import Contact from "./pages/Contact";
+import Booking from "./pages/Booking";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NewTicket from "./pages/NewTicket";
+import Profile from "./pages/Profile";
+import Tickets from "./pages/Tickets";
+import SingleTicketItem from "./pages/SingleTicketItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="container2">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/bookNow" element={<BookNow />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/Profile" element={<Profile />} />
+
+            <Route path="/new-ticket" element={<NewTicket />} />
+
+            <Route path="/tickets" element={<Tickets />} />
+
+            <Route path="/ticket/:ticketId" element={<SingleTicketItem />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
