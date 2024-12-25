a0ee389e39e7430c37baf443fe2ed94502b46870
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2024-12-25 10:22:39 +0000

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,3 +146,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
