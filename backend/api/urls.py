661e25797918e81fddfb20b7d909d97551fd1df0
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-06 11:08:35 +0000

    update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,7 @@ from .api import ApptViewSet
 router = routers.DefaultRouter()
 router.register("api/appts", ApptViewSet, "appts")
 
+# that'll basically just give us the URLa that were registered for this endpoint
 urlpatterns = router.urls
+
+# We should have a basoc CRUD API
